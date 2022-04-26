52644758ed3dc0581948ad62a1a904fc5c82d296
jamesnathanan <57458881+jamesnathanan@users.noreply.github.com>
2022-04-27 03:03:10 +0700

    version 1
    try async version of for loop convertor

---
+++ AsyncConvertToPNG.js
@@ -0,0 +1,56 @@
+// npm i --save jimp
+
+const Jimp = require('jimp');
+const { readdirSync, rename } = require('fs');
+const { resolve } = require('path');
+
+// Get path to image directory
+//const imageDirPath = resolve(__dirname, './MVRC_Aliens');
+
+// Get an array of the files inside the folder
+//const files = readdirSync(imageDirPath);
+
+// Loop through each file that was retrieved
+// files.forEach(file => {
+// 	let fileName = file
+// 	Jimp.read(`./MVRC_Aliens/${fileName}`, function(err, image) {
+// 		if(err) {
+// 			console.log(err)
+// 		} else {
+// 			image.write( `./png/${file}`)	
+// 		}
+	
+// 	})
+// 	//console.log(file)
+// 	});
+
+const writeFile = (file) => {
+  Jimp.read(`./MVRC_Aliens/${file}.jpg`, function (err, image) {
+  //If there is an error in reading the image, 
+  //we will print the error in our terminal
+  if (err) {
+    console.log(err)
+  } else {
+    image.write(`./test/${file}.png`)
+    console.log(`image ${file} was created!`)
+    }
+  
+})
+}
+
+async function doWork(num) {
+  try {
+    const response = await writeFile(num)
+    console.log("Task was done !")
+  } catch (err) {
+    console.log(err)
+  }
+  
+}
+
+for (let i = 1; i < 11; i++) {
+    doWork(i);
+
+	}
+
+
