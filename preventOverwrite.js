aa918a12a0d9ffd34e64df3febecbf00822d99c0
jamesnathanan <57458881+jamesnathanan@users.noreply.github.com>
2023-04-30 23:01:37 +0000

    Update preventOverwrite.js

--- preventOverwrite.js
+++ preventOverwrite.js
@@ -31,13 +31,32 @@ So, in the resulting dependentTraits object, "Eye color/Cyan" will have the valu
 
 
 
+// function mergeObjects(...objects) {
+//   const result = {};
+
+//   for (const obj of objects) {
+//     for (const key in obj) {
+//       if (result.hasOwnProperty(key)) {
+//         result[key] = result[key].concat(obj[key]);
+//       } else {
+//         result[key] = obj[key];
+//       }
+//     }
+//   }
+
+//   return result;
+// }
+
 function mergeObjects(...objects) {
   const result = {};
 
   for (const obj of objects) {
+    console.log('Merging object:', obj);
+
     for (const key in obj) {
       if (result.hasOwnProperty(key)) {
-        result[key] = result[key].concat(obj[key]);
+        const values = new Set([...result[key], ...obj[key]]);
+        result[key] = Array.from(values);
       } else {
         result[key] = obj[key];
       }
@@ -46,6 +65,24 @@ function mergeObjects(...objects) {
 
   return result;
 }
+/*
+  prevent this result
+'Head Gear/Red Mohawk (C)': [
+    'Big Hats/None',
+    'Balaclava/Black',
+    'Balaclava/White',
+    'Balaclava/Red Yellow',
+    'Balaclava/Red',
+    'Head Bandana/None',
+    'Big Hats/None',
+    'Balaclava/Black',
+    'Balaclava/White',
+    'Balaclava/Red Yellow',
+    'Balaclava/Red'
+  ],
+  you can improve the mergeObjects() function to prevent repetitive values. One way to achieve this is by using a Set to store unique values instead of an array. 
+  In this implementation, we use the spread operator to concatenate the arrays in result[key] and obj[key], then create a new Set to remove duplicate values. Finally, we convert the Set back to an array using Array.from() and store it in result[key]. This ensures that the final object contains only unique values for each key.
+*/
 
 const incompatibleTraits = mergeObjects(
   headGearIn,
