d910940bc692b5643885f8a7afa9dc4555d229b5
jamesnathanan <57458881+jamesnathanan@users.noreply.github.com>
2023-04-30 21:07:34 +0000

    Update preventOverwrite.js

--- preventOverwrite.js
+++ preventOverwrite.js
@@ -1,3 +1,36 @@
+// Problem
+const eyeColorCyan = {
+  "Eye color/Cyan": [
+    "Eyeball/White",
+  ],
+};
+
+const backgroundBlue = {
+  "Eye color/Cyan": [
+    "Eyeball/Red",
+    "Top lid/low"
+  ],
+};
+
+const dependentTraits = {
+  ...eyeColorCyan,
+  ...backgroundBlue,
+};
+
+console.log(dependentTraits);
+// Output: { "Eye color/Cyan": ["Eyeball/Red", "Top lid/low"] }
+/*
+If two objects have the same key name and you use the spread operator to combine them as in your example, the later object's value will overwrite the previous one.
+
+In your example, both eyeColorCyan and backgroundBlue objects have a property with the key name "Eye color/Cyan". When you combine them using the spread operator, the value of "Eye color/Cyan" in backgroundBlue object will overwrite the value of "Eye color/Cyan" in eyeColorCyan object.
+
+So, in the resulting dependentTraits object, "Eye color/Cyan" will have the value ["Eyeball/Red", "Top lid/low"], not ["Eyeball/White"] as in eyeColorCyan.
+
+*/
+
+
+
+
 function mergeObjects(...objects) {
   const result = {};
 
